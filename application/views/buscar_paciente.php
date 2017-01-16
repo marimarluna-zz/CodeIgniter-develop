41e7cd63f134d8d9f4f49198198dda79ddcdd738
marimarluna <marimar.lunam@gmail.com>
2017-01-16 14:32:48 -0400

    Update buscar_paciente.php

--- application/views/buscar_paciente.php
+++ application/views/buscar_paciente.php
@@ -53,6 +53,11 @@ $(document).ready( function() {
             </div>
           </div>
           <br>
+						
+						<input  style = "display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+							  <input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
+							  <input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+							  <input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
          
             <br><br>
           <div class="col-sm-offset-4">
