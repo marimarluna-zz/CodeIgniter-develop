b9cd80d44c1be241d0f91ebe718dad0efe01ab2a
marimarluna <marimar.lunam@gmail.com>
2017-01-16 13:42:49 -0400

    Update v_tabla_especialista.php

--- application/views/v_tabla_especialista.php
+++ application/views/v_tabla_especialista.php
@@ -45,10 +45,10 @@ $(document).ready( function() {
                     <div class="row col-sm-1"> 
                         <button type="submit" value='login' name="datos" class="btn btn-success" > <span class="glyphicon glyphicon glyphicon-search"> </span></button>
                     </div>
-																	<input  style = "display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-							  <input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
-							  <input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-							  <input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
+																	<input type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+							  <input type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
+							  <input type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+							  <input type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
                 </form>
                 <form action="<?php echo base_url(); ?>especialista_controller/tabla">
                     <div class="col-sm-1 col-md-offset-1" style="margin-top:-3%"> 
