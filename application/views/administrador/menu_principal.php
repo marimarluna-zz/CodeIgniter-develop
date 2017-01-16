f5c1a3eb2c4b65cf9873686d0adbdceab132c35b
marimarluna <marimar.lunam@gmail.com>
2017-01-16 10:06:38 -0400

    Update menu_principal.php

--- application/views/administrador/menu_principal.php
+++ application/views/administrador/menu_principal.php
@@ -31,10 +31,10 @@
             <form action="<?php echo base_url(); ?>especialista_controller/tabla">
               <div class="col-sm-4 col-md-offset-4 " >
                  <button class="btn btn-lg btn-danger btn-block" type="submit" name="submit" value="login"><span class="glyphicon glyphicon-check"></span> Busqueda de Especialistas </button>
-		 <input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-		 <input style="display:none" type="text" id="clave" name="clave" value="<?php echo $clave; ?>"  class="form-control" >
-		 <input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-		 <input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
+		 <input type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+		 <input type="text" id="clave" name="clave" value="<?php echo $clave; ?>"  class="form-control" >
+		 <input type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+		 <input type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
               </div>
             </form>
             </div>
