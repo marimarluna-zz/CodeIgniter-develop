9f2054b675a8cafbef78a8c779bcdebaac1a9232
marimarluna <marimar.lunam@gmail.com>
2017-01-16 14:47:26 -0400

    Update v_tabla_administrador.php

--- application/views/v_tabla_administrador.php
+++ application/views/v_tabla_administrador.php
@@ -41,11 +41,21 @@ $(document).ready( function() {
                             <option value="ci_administrador" >Cedula</option>
                        </select>
                   </div>
+			<input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+					<input style="display:none" type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
+					<input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+					<input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" > 
+
                     <div class="row col-sm-1"> 
                         <button type="submit" value='login' name="datos" class="btn btn-success" > <span class="glyphicon glyphicon glyphicon-search"> </span></button>
                     </div>
                 </form>
-                <form action="<?php echo base_url(); ?>especialista_controller/tabla">
+                <form action="<?php echo base_url(); ?>especialista_controller/tabla" method="POST">
+			<input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+					<input style="display:none" type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
+					<input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+					<input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" > 
+
                     <div class="col-sm-1 col-md-offset-1" style="margin-top:-3%"> 
                             <button type="submit" value='login' name="datos" class="btn btn-danger" > Limpiar </button>
                     </div>
@@ -76,10 +86,10 @@ $(document).ready( function() {
                                     <div class="" style="display:none">
                                         <input type="text" min="1" id="ci_administrador" name="ci_administrador" value="<?php echo $deptlist[$i]->ci_administrador;?>" class="form-control col-sm-2"> 
                                     </div>
-																																	<input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-																																	<input style="display:none" type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
-																																	<input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-																																	<input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" > 
+						<input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+						<input style="display:none" type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
+						<input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+						<input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" > 
                                     <div class="row col-sm-3" style="margin-left:5%"> 
                                         <button type="submit" value='login' name="datos" class="btn btn-success" > <span class="glyphicon glyphicon-user"> </span></button>
                                     </div>
