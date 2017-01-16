21f68c7d5127edd4534df92e04b2c27051ee18a3
marimarluna <marimar.lunam@gmail.com>
2017-01-16 10:19:11 -0400

    Update menu_principal.php

--- application/views/administrador/menu_principal.php
+++ application/views/administrador/menu_principal.php
@@ -31,10 +31,10 @@
             <form action="<?php echo base_url(); ?>especialista_controller/tabla" method="POST">
               <div class="col-sm-4 col-md-offset-4 " >
                  <button class="btn btn-lg btn-danger btn-block" type="submit" name="submit" value="login"><span class="glyphicon glyphicon-check"></span> Busqueda de Especialistas </button>
-		 <input type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-		 <input type="text" id="clave" name="clave" value="<?php echo $clave; ?>"  class="form-control" >
-		 <input type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-		 <input type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
+		 <input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+		 <input style="display:none" type="text" id="clave" name="clave" value="<?php echo $clave; ?>"  class="form-control" >
+		 <input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+		 <input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
               </div>
             </form>
             </div>
@@ -44,7 +44,7 @@
 
       <div class="container">
         <div class="row">
-            <form action="<?php echo base_url(); ?>especialista_controller">
+            <form action="<?php echo base_url(); ?>especialista_controller" method="POST">
               <div class="col-sm-4 col-md-offset-4 " >
                  <button class="btn btn-lg btn-success btn-block" type="submit" name="submit" value="login"><span class="glyphicon glyphicon-check"></span> Registrar Especialista </button>
 		 <input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
@@ -60,7 +60,7 @@
 
     <div class="container">
         <div class="row">
-            <form action="<?php echo base_url(); ?>administrador_controller">
+            <form action="<?php echo base_url(); ?>administrador_controller" method="POST">
               <div class="col-sm-4 col-md-offset-4 " >
                  <button class="btn btn-lg btn-success btn-block" type="submit" name="submit" value="login"><span class="glyphicon glyphicon-check"></span> Registrar Administrador </button>
 		 <input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
