5ef4f939c6beaf9cd707134c1e7b572dd9f1b1af
marimarluna <marimar.lunam@gmail.com>
2017-01-16 10:38:31 -0400

    Update menu_principal.php

--- application/views/especialista/menu_principal.php
+++ application/views/especialista/menu_principal.php
@@ -31,7 +31,11 @@
             <form action="<?php echo base_url(); ?>paciente_controller/tabla">
               <div class="col-sm-4 col-md-offset-4 " >
                  <button class="btn btn-lg btn-danger btn-block" type="submit" name="submit" value="login"><span class="glyphicon glyphicon-check"></span> Busqueda de Pacientes </button>
-              </div>
+             				<input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+																 <input style="display:none" type="text" id="clave" name="clave" value="<?php echo $clave; ?>"  class="form-control" >
+		 														<input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+																 <input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" > 
+													</div>
             </form>
             </div>
       </div>  
@@ -43,6 +47,10 @@
             <form action="<?php echo base_url(); ?>paciente_controller">
               <div class="col-sm-4 col-md-offset-4 " >
                  <button class="btn btn-lg btn-success btn-block" type="submit" name="submit" value="login"><span class="glyphicon glyphicon-check"></span> Registrar Paciente </button>
+																<input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+																 <input style="display:none" type="text" id="clave" name="clave" value="<?php echo $clave; ?>"  class="form-control" >
+		 														<input style="display:none" type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+																 <input style="display:none" type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" > 
               </div>
             </form>
             </div>
