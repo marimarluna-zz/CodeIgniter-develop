01ab576253fe73de775ded3aedce85bd67ab5f62
marimarluna <marimar.lunam@gmail.com>
2017-01-16 14:30:06 -0400

    Update Usuario_controller.php

--- application/controllers/Usuario_controller.php
+++ application/controllers/Usuario_controller.php
@@ -14,31 +14,16 @@ class Usuario_controller extends CI_Controller {
  {
    $this->load->helper(array('form'));
 
-   $session_data = $this->session->userdata('logged_in');
-   $data['username'] = $session_data['username'];
-   $data['id'] = $session_data['id'];
-   //$data['nombre'] = $session_data['nombre'];
-   //$data['apellido'] = $session_data['apellido'];
+   $data['usuario'] = $this->input->post('usuario');
+       $data['password'] = $this->input->post('clave');
+       $data['perfil'] = $this->input->post('perfil');
+       $data['id'] = $this->input->post('id');
 
    $this->load->view('menu_navegacion_admin', $data);
    $this->load->view('c_usuario', $data);
  }
 
  function guardar(){
-
-
-  $this->load->library('form_validation');
-  // field name, error message, validation rules
-  //$this->form_validation->set_rules('nombre', 'El nombre ', 'trim|required|min_length[4]|xss_clean');
-  //$this->form_validation->set_rules('apellido', 'El apellido', 'trim|required|min_length[4]|xss_clean');
-  //$this->form_validation->set_rules('ci_usuario', 'ci_usuario', 'trim|required|min_length[4]|xss_clean');
- 
-  if($this->form_validation->run() == FALSE)
-  {
-   $this->index();
-  }
-  else
-  {
 
     $ci_usuario = $this->input->post('id');
 
@@ -52,9 +37,10 @@ class Usuario_controller extends CI_Controller {
 
   if ($query -> num_rows() != 0 ){
 
-   $session_data = $this->session->userdata('logged_in');
-   $data['username'] = $session_data['username'];
-   $data['id'] = $session_data['id'];
+   $data['usuario'] = $this->input->post('usuario');
+       $data['password'] = $this->input->post('clave');
+       $data['perfil'] = $this->input->post('perfil');
+       $data['id'] = $this->input->post('id');
    
    $data['error'] = "Ya existe un usuario con esa cedula";
 
@@ -66,24 +52,24 @@ class Usuario_controller extends CI_Controller {
      $this->load->model('usuario_model');
      $this->usuario_model->guardar();
 
-     $session_data = $this->session->userdata('logged_in');
-     $data['username'] = $session_data['username'];
-     $data['id'] = $session_data['id'];
+     $data['usuario'] = $this->input->post('usuario');
+       $data['password'] = $this->input->post('clave');
+       $data['perfil'] = $this->input->post('perfil');
+       $data['id'] = $this->input->post('id');
      
 
      $this->load->view('menu_navegacion_admin', $data);
      $this->load->view('usuario_cargado');
    }
-  }
 
  }
 
  function editar(){
 
-  $session_data = $this->session->userdata('logged_in');
-     $data['username'] = $session_data['username'];
-     $data['id'] = $session_data['id'];
-     
+  $data['usuario'] = $this->input->post('usuario');
+       $data['password'] = $this->input->post('clave');
+       $data['perfil'] = $this->input->post('perfil');
+       $data['id'] = $this->input->post('id');
 
      $this->load->view('menu_navegacion_admin', $data);
      $this->load->view('e_especialista');
@@ -93,8 +79,13 @@ class Usuario_controller extends CI_Controller {
 
    $this->load->model('usuario_model');
    $thisusuario_model->tabla_usuario();
+		
+		 $data['usuario'] = $this->input->post('usuario');
+       $data['password'] = $this->input->post('clave');
+       $data['perfil'] = $this->input->post('perfil');
+       $data['id'] = $this->input->post('id');
 
-   $this->load->view('v_tabla_usuario');
+   $this->load->view('v_tabla_usuario', $data);
 
  }
 
@@ -184,8 +175,10 @@ class Usuario_controller extends CI_Controller {
 
         $data['pagination'] = $this->pagination->create_links();
 
-           $session_data = $this->session->userdata('logged_in');
-           $data['username'] = $session_data['username'];
+           $data['usuario'] = $this->input->post('usuario');
+       $data['password'] = $this->input->post('clave');
+       $data['perfil'] = $this->input->post('perfil');
+       $data['id'] = $this->input->post('id');
            
 
         //load the department_view
@@ -193,75 +186,6 @@ class Usuario_controller extends CI_Controller {
         $this->load->view('v_tabla_usuario',$data);      
    
     }
-
-     /* function eliminar(){
-
-     $session_data = $this->session->userdata('logged_in');
-     $id = $session_data['id'];
-
-     $query = $this->db->query(' SELECT * FROM `usuario` WHERE id = "'.$id.'"');
-     $sql = $query->row();
-     
-     $vis = $sql->tipo;
-
-    if($vis == 1){
-
-    $this->load->model('especialista_model');
-    $this->causante_model->eliminar();
-    $data['error'] = 'El Especialista ha sido eliminado exitosamente';
-
-    } else {
-
-      $data['error'] = 'Como usuario no posee el permiso para eliminar';
-      
-    }
-
-        //pagination settings
-        $config['base_url'] = site_url('causante_controller/tabla');
-        $config['total_rows'] = $this->db->count_all('causante');
-        $config['per_page'] = "7";
-        $config["uri_segment"] = 3;
-        $choice = $config["total_rows"] / $config["per_page"];
-        $config["num_links"] = floor($choice);
-
-        //config for bootstrap pagination class integration
-        $config['full_tag_open'] = '<ul class="pagination">';
-        $config['full_tag_close'] = '</ul>';
-        $config['first_link'] = false;
-        $config['last_link'] = false;
-        $config['first_tag_open'] = '<li>';
-        $config['first_tag_close'] = '</li>';
-        $config['prev_link'] = '&laquo';
-        $config['prev_tag_open'] = '<li class="prev">';
-        $config['prev_tag_close'] = '</li>';
-        $config['next_link'] = '&raquo';
-        $config['next_tag_open'] = '<li>';
-        $config['next_tag_close'] = '</li>';
-        $config['last_tag_open'] = '<li>';
-        $config['last_tag_close'] = '</li>';
-        $config['cur_tag_open'] = '<li class="active"><a href="#">';
-        $config['cur_tag_close'] = '</a></li>';
-        $config['num_tag_open'] = '<li>';
-        $config['num_tag_close'] = '</li>';
-
-        $this->pagination->initialize($config);
-        $data['page'] = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-
-        //call the model function to get the department data
-        $data['deptlist'] = $this->causante_model->tabla_p($config["per_page"], $data['page']);           
-
-        $data['pagination'] = $this->pagination->create_links();
-
-           $session_data = $this->session->userdata('logged_in');
-           $data['nickname'] = $session_data['nickname'];
-           $data['id'] = $session_data['id'];
-           $data['nombre'] = $session_data['nombre'];
-           $data['apellido'] = $session_data['apellido'];
-
-        //load the department_view
-        $this->load->view('menu_navegacion_admin', $data);
-        $this->load->view('v_tabla_causantes',$data);
- } */
 
  
 }
