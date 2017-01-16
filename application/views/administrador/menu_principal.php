2c65ee194ee13cabb3ce7fccbf2ea65f9e30b590
marimarluna <marimar.lunam@gmail.com>
2017-01-16 09:19:30 -0400

    Update menu_principal.php

--- application/views/administrador/menu_principal.php
+++ application/views/administrador/menu_principal.php
@@ -28,7 +28,7 @@
 
       <div class="container">
         <div class="row">
-            <form action="<?php echo base_url(); ?>Especialista_controller/tabla">
+            <form action="<?php echo base_url(); ?>especialista_controller/tabla">
               <div class="col-sm-4 col-md-offset-4 " >
                  <button class="btn btn-lg btn-danger btn-block" type="submit" name="submit" value="login"><span class="glyphicon glyphicon-check"></span> Busqueda de Especialistas </button>
 		 <input style="display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
