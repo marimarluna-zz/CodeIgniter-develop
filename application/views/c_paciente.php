729443759175cb5f9f47ebd084d82932d38bcf36
marimarluna <marimar.lunam@gmail.com>
2017-01-19 18:20:34 -0400

    Update c_paciente.php

--- application/views/c_paciente.php
+++ application/views/c_paciente.php
@@ -73,6 +73,11 @@ form {float:right;}
                <div class="col-sm-12 col-md-offset-2 form-group">
                 <div class="col-sm-4 form-group">
                 <label>Cedula</label>
+			 <select name="n" id="n">
+                  <option value="" >Seleccione</option>
+                  <option value="V" >V</option>
+                  <option value="E" >E</option>
+                </select>
                 <input type="number" min="0" id="ci_paciente" name="ci_paciente" placeholder="Cedula" class="form-control">
               </div>
               <div class="col-sm-4 form-group">
