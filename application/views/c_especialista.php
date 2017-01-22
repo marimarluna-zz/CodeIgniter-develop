ab000bd52b3ea4cca2b3c4d829bbdd2df482e6b2
marimarluna <marimar.lunam@gmail.com>
2017-01-21 23:11:28 -0400

    Update c_especialista.php

--- application/views/c_especialista.php
+++ application/views/c_especialista.php
@@ -77,9 +77,9 @@ form {float:right;}
                   <option value="V" >V</option>
                   <option value="E" >E</option>
                 </select>
+		</div>
 	       <div class="col-sm-4 form-group">
                 <input type="number" min="0" id="ci_especialista" name="ci_especialista" placeholder="Cedula" class="form-control">
-              </div>
 	      </div>
               <div class="col-sm-4 form-group">
                 <label>Telefono</label>
