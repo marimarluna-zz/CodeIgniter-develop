642f0b8365784d30482016578b5ed4c30d6ed986
marimarluna <marimar.lunam@gmail.com>
2017-01-22 14:24:42 -0400

    Update menu_navegacion_espe.php

--- application/views/menu_navegacion_espe.php
+++ application/views/menu_navegacion_espe.php
@@ -19,41 +19,7 @@
     
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Home</a></li>
-            <li><form name="form_prueba" action="<?= base_url().'Paciente_controller'?>" method="POST">
-		    
-			<input style = "display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-			<input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
-			<input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-			<input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
-		    
-			<button type="submit" value='login' style="margin-top:9%" name="datos" class="btn btn-info">Crear Paciente</button>    
-		</form></li>
-            <li><form name="form_prueba" action="<?= base_url().'paciente_controller/tabla'?>" method="POST">
-		  <input style = "display:none"  type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-		  <input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
-		  <input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-		  <input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
-			<button type="submit" value='login' style="margin-top:9%" name="datos" class="btn btn-info">Lista de pacientes</button>    
-		</form></li>
-            <li>			
-		<form name="form_prueba" action="<?= base_url().'paciente_controller/buscar_paciente'?>" method="POST"><input  style = "display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-							  <input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
-							  <input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-							  <input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
-			<button type="submit" value='login' style="margin-top:9%" name="datos" class="btn btn-info">Buscar Paciente</button>    
-		</form>				
-	    </li>
-	    <li>
-		<h5><a><b><span class="glyphicon glyphicon-user"></span> <?php echo $usuario; ?></b></a></h5>
-	    </li>
-	    <li>
-	        <a href="<?php echo base_url(); ?>login"><b>Salir <span class="glyphicon glyphicon-share-alt"></span></b></a>
-	    </li>
-          </ul>
-		
-		
+          
           <ul class="nav navbar-nav navbar-right">
             <div class="row col-sm-12">
 		    <div class="col-sm-3">
