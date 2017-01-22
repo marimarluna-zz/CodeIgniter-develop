5606b22b16895e8bea135692cfe74c74c0c1160a
marimarluna <marimar.lunam@gmail.com>
2017-01-22 16:10:43 -0400

    Update menu_navegacion_espe.php

--- application/views/menu_navegacion_espe.php
+++ application/views/menu_navegacion_espe.php
@@ -18,7 +18,7 @@
 							  <input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
 							  <input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
 							  <input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
-			<button type="submit" value='login' style="margin-top:9%" name="datos" class="btn btn-info">Inicio</button>    
+			<button type="submit" value='login' style="margin-top:9%" name="datos" class="btn btn-info"><img src="<?=base_url().'assents/img/nhisres100.png'?>" style="max-width:40px"></button>    
 		</form>	
         </div>
     
