945cc03b403fcbe4d173c600676e7e5fd16b5819
marimarluna <marimar.lunam@gmail.com>
2017-01-22 15:38:28 -0400

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,25 +5,9 @@ class Login extends CI_Controller {
  public function index()
  {
 
-  $perfil = $this->input->post('perfil');
-		
-		$data['usuario'] = $this->input->post('usuario');
-       $data['password'] = $this->input->post('clave');
-       $data['perfil'] = $this->input->post('perfil');
-       $data['id'] = $this->input->post('id');
-		
-		
-		if($perfil == 'Administrador'){
-      $this->load->view('menu_navegacion_admin', $data);
-        $this->load->view('administrador/menu_principal', $data);
-     }else{
-      $this->load->view('menu_navegacion_espe', $data);
-        $this->load->view('especialista/menu_principal', $data);
-     }
- 
- }
- 
-
+        $this->load->view('login');
+	 
+}
 
 
 }
