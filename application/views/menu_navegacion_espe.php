7c996bdc00900776645d64198c5116257337cc62
marimarluna <marimar.lunam@gmail.com>
2017-01-22 03:12:35 -0400

    Update menu_navegacion_espe.php

--- application/views/menu_navegacion_espe.php
+++ application/views/menu_navegacion_espe.php
@@ -23,7 +23,7 @@
             <li><a href="#">Home</a></li>
             <li><form name="form_prueba" action="<?= base_url().'Paciente_controller'?>" method="POST">
 		    
-			<input  style = "display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+			<input style = "display:none" type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
 			<input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
 			<input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
 			<input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
@@ -31,10 +31,10 @@
 			<button type="submit" value='login' style="margin-top:9%" name="datos" class="btn btn-info">Crear Paciente</button>    
 		</form></li>
             <li><form name="form_prueba" action="<?= base_url().'paciente_controller/tabla'?>" method="POST">
-		    <input   type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
-		  <input   type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
-		  <input   type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
-		  <input   type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
+		  <input style = "display:none"  type="text" id="usuario" name="usuario" value="<?php echo $usuario; ?>"  class="form-control" >
+		  <input style = "display:none"  type="text" id="clave" name="clave" value="<?php echo $password; ?>"  class="form-control" >
+		  <input style = "display:none"  type="text" id="perfil" name="perfil" value="<?php echo $perfil; ?>"  class="form-control" >
+		  <input style = "display:none"  type="text" id="id" name="id" value="<?php echo $id; ?>"  class="form-control" >
 			<button type="submit" value='login' style="margin-top:9%" name="datos" class="btn btn-info">Lista de pacientes</button>    
 		</form></li>
             <li>			
