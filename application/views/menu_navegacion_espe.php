462b9e8e95587ec61d18adce66df6a1f1f19a45c
marimarluna <marimar.lunam@gmail.com>
2017-01-22 15:41:47 -0400

    Update menu_navegacion_espe.php

--- application/views/menu_navegacion_espe.php
+++ application/views/menu_navegacion_espe.php
@@ -14,7 +14,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="<?php echo base_url(); ?>inicio" >Inicio</a>
+          <a class="navbar-brand" href="<?php echo base_url(); ?>login/inicio" >Inicio</a>
         </div>
     
         <!-- Collect the nav links, forms, and other content for toggling -->
