93a796a75367e332c93841af846f4f9622e93403
marimarluna <marimar.lunam@gmail.com>
2017-01-22 14:16:57 -0400

    Update menu_navegacion_espe.php

--- application/views/menu_navegacion_espe.php
+++ application/views/menu_navegacion_espe.php
@@ -81,7 +81,7 @@
 		</form>		
 		    </div>
 		    <div class="col-sm-1">	 
-		    	<h5><a><b><span class="glyphicon glyphicon-user"></span> <?php echo $usuario; ?></b></a></h5>
+		    	<a><b><span class="glyphicon glyphicon-user"></span> <?php echo $usuario; ?></b></a>
 		    </div>
 		    <div class="col-sm-1">
 			<a href="<?php echo base_url(); ?>login"><b><span class="glyphicon glyphicon-share-alt"></span>Salir</b></a>    
