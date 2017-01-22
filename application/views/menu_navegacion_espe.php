64f58d6df08cbc0c13726c78fa927ce22fc86978
marimarluna <marimar.lunam@gmail.com>
2017-01-22 01:28:08 -0400

    Update menu_navegacion_espe.php

--- application/views/menu_navegacion_espe.php
+++ application/views/menu_navegacion_espe.php
@@ -24,7 +24,7 @@
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Works</a></li>
-            <li><a href="#">News</a></li>
+            <li><a href="#" class="btn btn-default">I'm a link button!</a></li>
             <li><a href="#">Contact</a></li>
             <li>
               <a class="btn btn-default btn-outline btn-circle collapsed"  data-toggle="collapse" href="#nav-collapse1" aria-expanded="false" aria-controls="nav-collapse1">Categories</a>
